864d814ecd4328e77114163f9869259dd7af8d76
Carlos <carlos.araya@gmail.com>
2022-03-06 01:26:40 -0800

    removed README and CONTRIBUTING. Updated LICENSE

--- src/config/lintstagedrc.js
+++ src/config/lintstagedrc.js
@@ -1,12 +1,13 @@
-const {resolveRwScripts, resolveBin} = require('../utils');
+const {resolveRwScripts} = require('../utils');
+// const {resolveRwScripts, resolveBin} = require('../utils');
 
 const RwScripts = resolveRwScripts();
-const doctoc = resolveBin('doctoc');
+// const doctoc = resolveBin('doctoc');
 
 module.exports = {
-  'README.md': [`${doctoc} --maxlevel 3 --notitle`],
+  // 'README.md': [`${doctoc} --maxlevel 3 --notitle`],
   '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|gql|graphql|mdx|vue)': [
-    `${RwScripts} format`,
+    // `${RwScripts} format`,
     `${RwScripts} lint`,
     `${RwScripts} test --findRelatedTests`,
   ],
